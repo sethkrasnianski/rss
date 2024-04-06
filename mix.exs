84b78232ac627b0367ac0c63bd3e5855d8e6b324
Seth Krasnianski <seth@mojotech.com>
2024-04-06 18:54:38 -0400

    fix: Bump ecto_sql to prevent compilation error

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Rss.MixProject do
       {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.0-rc.0", override: true},
       {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
+      {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
